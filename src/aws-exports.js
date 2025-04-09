c8ddca0c1ffcebd842fa8d9663aef7599160c7bf
Ravindra Warnasooriya <warnasooriya.ravindra@gmail.com>
2025-04-09 10:37:58 +0530

    config changed cognito

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,6 +2,6 @@
 export const awsExports={
     "aws_project_region": "ap-southeast-1",
     "aws_cognito_region": "ap-southeast-1",
-    "aws_user_pools_id": "ap-southeast-1_QFFSnvmcI",
-    "aws_user_pools_web_client_id": "2p1cbvielcm6telhfap8bne9vr"
+    "aws_user_pools_id": "ap-southeast-1_16DsoLziH",
+    "aws_user_pools_web_client_id": "402k4t3utf8vph9f6gfva32mpa"
 }
