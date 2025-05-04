94dea61e5e417a5dc7d79af7d5e8bd0cf6ead6ab
Ravindra Warnasooriya <warnasooriya.ravindra@gmail.com>
2025-05-04 15:11:00 +0530

    updatate cognito user pool

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,6 +2,6 @@
 export const awsExports={
     "aws_project_region": "ap-southeast-1",
     "aws_cognito_region": "ap-southeast-1",
-    "aws_user_pools_id": "ap-southeast-1_16DsoLziH",
-    "aws_user_pools_web_client_id": "402k4t3utf8vph9f6gfva32mpa"
+    "aws_user_pools_id": "ap-southeast-1_pEHikfs7B",
+    "aws_user_pools_web_client_id": "7ef7oiac3lmac7qkqdfvgrbc37"
 }
